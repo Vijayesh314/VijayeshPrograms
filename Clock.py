41cf08442bc3a4b121870e1c35fc9e55edb286cc
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2024-03-21 13:56:42 -0700

    Create Clock.py
    This is a basic digital clock. It tells the time in hours, minutes, and seconds. However, this is in 24-hour time, not 12-hour time.

---
+++ Clock.py
@@ -0,0 +1,23 @@
+import datetime
+
+seconds=datetime.datetime.now().second + 1
+minutes=datetime.datetime.now().minute
+hours=datetime.datetime.now().hour
+
+import time
+from turtle import *
+t1=Turtle()
+while True:
+  t1.color('red')
+  t1.begin_fill()
+  t1.clear()
+  t1.write(str(hours).zfill(2) + ':' + str(minutes).zfill(2) + ':' + str(seconds).zfill(2), font=('oxygen', 25, 'normal'))
+  seconds=seconds+1
+  time.sleep(1)
+  if (seconds == 60):
+    seconds=0
+    minutes=minutes+1
+  if (minutes == 60):
+    minutes=0
+    hours=hours+1
+  t1.end_fill()
