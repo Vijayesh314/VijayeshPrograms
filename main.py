db42d4ab8b217ae564f9b89530f137f223f640d6
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2024-10-17 22:23:43 -0700

    Update mainturtle.py

--- main.py
+++ main.py
@@ -2,24 +2,37 @@ from myturtles import *
 import turtle as t
 import random
 
-'''shape = input("What shape do you want to have drawn? Circle, Triangle, Square, Pentagon, Hexagon: ")
-while shape.lower() not in ["circle", "triangle", "square", "pentagon", "hexagon"]:
+num = input("1) Circle \n2) Triangle \n3) Square \n4) Pentagon \n5) Hexagon\nEnter the number of the shape you want: ")
+while num.isnumeric() == False:
     print("Invalid input")
-    shape = input("What shape do you want to have drawn? Circle, Triangle, Square, Pentagon, Hexagon: ")
-user = shape.lower()'''
+    num = input("1) Circle \n2) Triangle \n3) Square \n4) Pentagon \n5) Hexagon\nEnter the number of the shape you want: ")
+shapeList = [circle, triangle, square, pentagon, hexagon]
+num = int(num) - 1
+
+def drawshape(x, y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    for i in range (6):
+        r = int(random.randrange(0,256))
+        g = int(random.randrange(0,256))
+        b = int(random.randrange(0,256))
+        shapeList[num](t, r, g, b)
+        t.penup()
+        t.goto(t.xcor()+125, t.ycor())
+        t.pendown()
 
 wn = t.Screen()
-wn.setup(width=900, height=900)
+wn.setup(width=800, height=800)
 t.colormode(255)
 t.speed(0)
+if num==1:
+    x = -300
+else:
+    x=-400
 
-for i in range (100):
-    r = int(random.randrange(0,256))
-    g = int(random.randrange(0,256))
-    b = int(random.randrange(0,256))
-    circle(t, r, g, b)
-    t.penup()
-    t.goto(random.randrange(-400, 400), random.randrange(-400, 400))
-    t.pendown()
+for i in range(5):
+    y = 200-int(i)*125
+    drawshape(x, y)
 
-wn.mainloop()
+t.mainloop()
