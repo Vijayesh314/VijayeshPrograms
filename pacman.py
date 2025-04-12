35603399c08d10992534bfbab7e9cb385fb78f2d
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2025-04-11 21:10:41 -0700

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -1,10 +1,11 @@
 import pgzrun
 from random import randint
-import tkinter
 from tkinter import Tk, messagebox, simpledialog
 import time
+import sys
 
 game_over = False
+lives = 3
 score = 0
 #Dimensions of screen
 HEIGHT = 800
@@ -12,6 +13,7 @@ WIDTH = 800
 
 root = Tk() 
 root.geometry("300x200")
+root.withdraw()
 
 #Define Pac Man
 pac=Actor("pac.png")
@@ -19,73 +21,89 @@ pac.pos = (100,100)
 #Define collectible
 pellet=Actor("pellet.png")
 pellet.pos = (200,200)
-#Define ghost
-ghost=Actor("ghost.png")
-ghost.pos = (800,100)
 
 #User chooses the number of ghosts following pac
-'''num = simpledialog.askinteger("Number of Ghosts", "How many ghosts do you want on the screen?")
-for i in range (num):
-    #Create multiple actors of the ghost'''
+num = simpledialog.askinteger("Input", "How many ghosts do you want?", minvalue=1, maxvalue=4)
+ghosts = []
+#Create ghosts based on user input
+for i in range(num):
+    ghost = Actor("ghost.png")
+    #Random starting position of ghosts
+    ghost.pos = (randint(600, 800), randint(100, 700))
+    ghosts.append(ghost)
 
 def draw():
     screen.fill("black")
     #Pac appears on screen
     pac.draw()
     pellet.draw()
-    ghost.draw()
+    for ghost in ghosts:
+        ghost.draw()
     screen.draw.text("Score: " + str(score), center=(50,50), color="white",fontsize=60)
+    screen.draw.text("Lives: " + str(lives), center=(50,100), color="white",fontsize=60)
 
-    if game_over:
-        screen.fill("red")
-        messagebox.showinfo("showinfo", "Final score" + str(score))
-
-def place_pellet():
-    pellet.x=randint(20, (WIDTH-20))
-    pellet.y=randint(20, (HEIGHT-20))
+def place_pellet(p):
+    p.x=randint(20, (WIDTH-20))
+    p.y=randint(20, (HEIGHT-20))
 
 def update():
-    global score
+    global game_over, lives, score
     #Left Arrow key pressed makes pac go left
-    if keyboard.left:
+    if keyboard.left or keyboard.a:
         pac.x=pac.x-4
     #Right Arrow key pressed makes pac go right
-    elif keyboard.right:
+    elif keyboard.right or keyboard.d:
         pac.x=pac.x+4
     #Up Arrow key pressed makes pac go up
-    elif keyboard.up:
+    elif keyboard.up or keyboard.w:
         pac.y=pac.y-4
     #Down Arrow key pressed makes pac go down
-    elif keyboard.down:
+    elif keyboard.down or keyboard.s:
         pac.y=pac.y+4
 
-    #Move ghost right if it's left of pac
-    if ghost.x < pac.x:
-        ghost.x += 1  
-    #Move ghost left if it's right of pac
-    elif ghost.x > pac.x:
-        ghost.x -= 1
-    #Move ghost down if its above pac
-    if ghost.y < pac.y:
-        ghost.y += 1
-    #Move ghost up if its below pac
-    elif ghost.y > pac.y:
-        ghost.y -= 1
+    for ghost in ghosts:
+        #Move ghost right if it's left of pac
+        if ghost.x < pac.x:
+            ghost.x += 1  
+        #Move ghost left if it's right of pac
+        elif ghost.x > pac.x:
+            ghost.x -= 1
+        #Move ghost down if its above pac
+        if ghost.y < pac.y:
+            ghost.y += 1
+        #Move ghost up if its below pac
+        elif ghost.y > pac.y:
+            ghost.y -= 1
 
-    #Score increases by 10 when pac collides with pellet
-    collected = pac.colliderect(pellet)
-    death = pac.colliderect(ghost)
-    if collected:
+    for ghost in ghosts:
+        #Conditionals of ghosts moving towards pac
+        if pac.colliderect(ghost):
+            #If pac collides with ghost, 1 life is lost
+            lives -= 1
+            #If pac collides with ghost thrice, the game is over
+            if lives == 0:
+                game_over = True
+                messagebox.showinfo("Game Over", f"Final score: {score}")
+                sys.exit()
+            #Game resests if pac collides with ghost
+            else:
+                pac.pos = (100, 100)
+                for ghost in ghosts:
+                    ghost.pos = (randint(600, 800), randint(100, 700))
+                messagebox.showinfo("Warning", f"You have {lives} lives left!")
+                time.sleep(1)
+    
+    #If pac collides with pellet, score increases by 10 the pellet is placed randomly on screen
+    if pac.colliderect(pellet):
         score += 10
-        place_pellet()
-
-    if death:
-        score -= 10
+        place_pellet(pellet)
 
 def time_up():
     global game_over
     game_over = True
 
-clock.schedule(time_up, 60.0)
-place_pellet()
+#Schedule the time up event after 60 seconds
+clock.schedule(time_up, 60)  
+
+place_pellet(pellet)
 pgzrun.go()
